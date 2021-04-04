d8e9f43cd64add9d3e0dd5a0e0f3d73c0beaf9dd
Niklas Metje <22395665+niklasmtj@users.noreply.github.com>
2021-04-04 14:09:25 +0200

    Updated allowed origins in cors

--- analytics-server/server.js
+++ analytics-server/server.js
@@ -7,10 +7,13 @@ const app = express()
 const port = 3000
 const GITHUB_URL = "https://github.com/niklasmtj/gh-analytics";
 
-app.use(cors())
+const corsOptions = {
+  origin: 'https://niklasmtj.github.io/gh-analytics',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 app.use(express.json())
 
-app.post("/analytics", async (req, res) => {
+app.post("/analytics", cors(corsOptions), async (req, res) => {
   const {pathName} = req.body;
   const f = pathName.split("/docs/")[1];
   await axios.get(GITHUB_URL + "/blob/main/docs/" + f);
